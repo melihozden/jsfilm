1dec03914d1676ccc5b4522f50cb986ef1c2ec87
melihozden <melihozden403@gmail.com>
2018-11-18 19:41:00 +0300

    saving storage and load from stroge

--- ui.js
+++ ui.js
@@ -43,3 +43,16 @@ UI.prototype.displayMessage = function(message,type){
   },1500);
 
 }
+
+UI.prototype.loadAllFilms = (films) =>{
+  const filmList = document.getElementById("films") ;
+
+  films.forEach((film)=>{ 
+    filmList.innerHTML += `<tr>
+    <td><img src="${film.url}" class="img-fluid img-thumbnail"></td>
+    <td>${film.title}</td>
+    <td>${film.director}</td>
+    <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
+  </tr>`
+    })
+}
